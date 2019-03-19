7a28ccc145c9f0f1ec1c5e4faf4e8a53ad317343
Shareck Renzo <47116341+renshareck@users.noreply.github.com>
2019-03-19 19:18:55 +0800

    Update large_file.py

--- read_large_file/large_file.py
+++ read_large_file/large_file.py
@@ -2,6 +2,8 @@ class read_large_file:
 
     def __init__(self):
         self.file_path = "large_file.txt"
+        self.file_path = "large_file_write.txt"
+        self.new_f = open(self.file_path, "r")
 
     def get_file(self):
         with open(self.file_path, 'rb') as f:
@@ -11,22 +13,24 @@ class read_large_file:
                     yield content
                 else:
                     return
-
-
-class write_largge_file:
-
-    def __init__(self):
-        self.file_path = "large_file_write.txt"
-        self.f = open(self.file_path, "a+")
+            '''
+            for line in f:
+                if line:
+                    yield line
+                else:
+                    return
+            '''
 
     def write_to_file(self, data):
-        self.f.write(data)
-        
-
-
+        self.new_f.write(data)
 
 
 if __name__ == "__main__":
     test = read_large_file()
+    i = 0
     for content in test.get_file():
-        print(content)
+        i = i + 1
+        test.write_to_file(content)
+        print i
+        
+    test.new_f.close()
